07ec4bfd3b97e6ec152dc6465257c6195c6ff664
Kirite11111 <eliott.garnier2810@gmail.com>
2023-03-06 18:38:19 +0100

    Question.java remplacement du Switch/Case de la fct appliqueEffet par des if/else if

--- src/main/Question.java
+++ src/main/Question.java
@@ -114,22 +114,19 @@ public class Question {
      * @param effets les effets de jauge à appliquer
      * @param personnage le personnage sur lequel les effets doivent être appliqués
      */
-    private void appliqueEffets(Map<TypeJauge,Integer> effets,
-                                Personnage personnage){
+    private void appliqueEffets(Map<TypeJauge,Integer> effets, Personnage personnage){
         for(Map.Entry<TypeJauge,Integer> effet : effets.entrySet()){
-            switch (effet.getKey()) {
-                case ARMEE -> personnage.getJaugeArmee().setValeur(
-                        personnage.getJaugeArmee().getValeur()
-                                + effet.getValue());
-                case CLERGE -> personnage.getJaugeClerge().setValeur(
-                        personnage.getJaugeClerge().getValeur()
-                                + effet.getValue());
-                case FINANCE -> personnage.getJaugeFinance().setValeur(
-                        personnage.getJaugeFinance().getValeur()
-                                + effet.getValue());
-                case PEUPLE -> personnage.getJaugePeuple().setValeur(
-                        personnage.getJaugePeuple().getValeur()
-                                + effet.getValue());
+            if (effet.getKey() == TypeJauge.ARMEE) {
+                personnage.getJaugeArmee().setValeur(personnage.getJaugeArmee().getValeur() + effet.getValue());
+            }
+            else if (effet.getKey() == TypeJauge.CLERGE) {
+                personnage.getJaugeClerge().setValeur(personnage.getJaugeClerge().getValeur() + effet.getValue());
+            }
+            else if (effet.getKey() == TypeJauge.FINANCE) {
+                personnage.getJaugeFinance().setValeur(personnage.getJaugeFinance().getValeur() + effet.getValue());
+            }
+            else if (effet.getKey() == TypeJauge.PEUPLE) {
+                personnage.getJaugePeuple().setValeur(personnage.getJaugePeuple().getValeur() + effet.getValue());
             }
         }
     }
